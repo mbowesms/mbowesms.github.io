903381298f5c017585ddc4bef9ce418a43471b74
mbowesms <56764313+mbowesms@users.noreply.github.com>
2020-03-27 08:45:12 -0400

    Add files via upload

---
+++ new/charts/api/v1/ess-complete/index.php
@@ -0,0 +1,23 @@
+<?php
+$mysqli = new mysqli("localhost", "tracker", "tracker", "tracker");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+/* Prepared statement, stage 1: prepare */
+if (!($stmt = $mysqli->prepare("SELECT * FROM con_ed_sir"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+/* Prepared statement, stage 2: bind and execute */
+if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+} else {
+    $result = $stmt->get_result();
+    $response = [];
+    foreach($result as $item)
+        array_push($response,$item);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+?>
